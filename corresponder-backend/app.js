318ecf8b4eeb91a045baeed9b72aea87be3adb0a
Nikodem <nikodem.lorenz@gmail.com>
2019-02-25 17:54:06 +0100

    Installed and configured babel for nodemon

--- corresponder-backend/app.js
+++ corresponder-backend/app.js
@@ -1,6 +1,11 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 
 app.get('/', (req, res) => {})
 
+
+export default {
+   asd: 'asd'
+}
 app.listen(3001, () => console.log(`server's up!`))
